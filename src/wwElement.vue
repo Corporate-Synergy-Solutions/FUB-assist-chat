5cd4c929387d584cc0e924f288857823ba1c86df
jamtechdev <jamtechba@gmail.com>
2025-07-16 15:02:43 +0530

    add tailwind css cdn

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -129,7 +129,7 @@ export default {
   }
 };
 </script>
-
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Google+Sans:wght@400;500&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap");
